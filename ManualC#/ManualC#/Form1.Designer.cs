8b009b4dab7908df9b74b3c7c67d10b3f29156b1
Quark <gennadiy.kaydalov@mail.ru>
2023-06-15 19:39:16 +0300

    UI First version

--- ManualC#/ManualC#/Form1.Designer.cs
+++ ManualC#/ManualC#/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ManualList = new System.Windows.Forms.ListBox();
+            this.Search = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ManualList
+            // 
+            this.ManualList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.ManualList.BackColor = System.Drawing.SystemColors.Control;
+            this.ManualList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ManualList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ManualList.Font = new System.Drawing.Font("Verdana", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ManualList.FormattingEnabled = true;
+            this.ManualList.ItemHeight = 18;
+            this.ManualList.Items.AddRange(new object[] {
+            "Операторы",
+            "Типы данных",
+            "Массивы",
+            "Библиотечные классы"});
+            this.ManualList.Location = new System.Drawing.Point(3, 35);
+            this.ManualList.MultiColumn = true;
+            this.ManualList.Name = "ManualList";
+            this.ManualList.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.ManualList.Size = new System.Drawing.Size(204, 596);
+            this.ManualList.TabIndex = 1;
+            this.ManualList.SelectedIndexChanged += new System.EventHandler(this.ManualList_SelectedIndexChanged);
+            // 
+            // Search
+            // 
+            this.Search.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search.ForeColor = System.Drawing.Color.Gray;
+            this.Search.Location = new System.Drawing.Point(3, 637);
+            this.Search.Name = "Search";
+            this.Search.Size = new System.Drawing.Size(204, 27);
+            this.Search.TabIndex = 2;
+            this.Search.Text = "Поиск";
+            this.Search.TextChanged += new System.EventHandler(this.Search_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(11, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(1023, 629);
+            this.textBox2.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(216, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1046, 673);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.ManualList);
+            this.panel2.Controls.Add(this.Search);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(210, 673);
+            this.panel2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Manual";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1023, 33);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Title";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1262, 673);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "C# Manual";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox ManualList;
+        private System.Windows.Forms.TextBox Search;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- ManualC#/ManualC#/Form1.cs
+++ ManualC#/ManualC#/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace ManualC_
 {
@@ -15,6 +16,16 @@ namespace ManualC_
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void ManualList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Search_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
